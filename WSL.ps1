6618e4bc53c18563352c1a802ead8de9f4914e25
Saho <147002941+Sahozen@users.noreply.github.com>
2025-03-01 10:27:58 +0100

    Update WSL.ps1

--- WSL.ps1
+++ WSL.ps1
@@ -4,3 +4,6 @@ powershell.exe
 -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "\\alphatech.local\data\Partage\IT\Scripts\Sauvegarde.ps1"
 
 \\alphatech.local\data\Partage\IT\Scripts\
+
+powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "\\alphatech.local\data\Partage\IT\Scripts\Sauvegarde.ps1"
+
