93ad0318f2cfdf0d61a986bb7330fc90ba6c29f3
Saho <147002941+Sahozen@users.noreply.github.com>
2025-03-09 23:18:24 +0100

    Update WSL.ps1

--- WSL.ps1
+++ WSL.ps1
@@ -7,8 +7,14 @@ powershell.exe
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
 
+-d Debian -- sudo service cron start
+
+
 powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "\\alphatech.local\data\Partage\IT\Scripts\Sauvegarde.ps1"
 
-0 2 * * * powershell.exe -File /mnt/o/NT/Scripts/Sauvegarde.ps1 >> /home/<votre_user>/sauvegarde.log 2>&1
+0 2 * * * powershell.exe -File "C:\Scripts\Sauvegarde.ps1" >> /home/<user>/sauvegarde.log 2>&1
+
+0 2 * * * /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File /mnt/c/Scripts/Sauvegarde.ps1 >> /home/<user>/sauvegarde.log 2>&1
+
 
 
